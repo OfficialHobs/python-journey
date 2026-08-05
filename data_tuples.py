8d9b98721277e68f2476becd620ae92ed95c90d8
Habib oluwa <oluwahabib27@gmail.com>
2026-08-05 19:01:35 +0100

    uncomment

--- data_tuples.py
+++ data_tuples.py
@@ -2,7 +2,7 @@
 my_list = [(i, j) for i in range(5) for j in range(4)]
 print(my_list)
 
-#creating a table with list
-# mylist = [[i for i in range (4)] for j in range (3)]
-# print (mylist)
+# creating a table with list
+mylist = [[i for i in range (4)] for j in range (3)]
+print (mylist)
 
