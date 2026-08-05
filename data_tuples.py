2c2b54cf45b5bf541f7bf338d29df93f8f2cfb0b
Habib oluwa <oluwahabib27@gmail.com>
2026-08-05 18:13:38 +0100

    tuple in python

---
+++ data_tuples.py
@@ -0,0 +1,8 @@
+#creating possibilities with list
+my_list = [(i, j) for i in range(5) for j in range(4)]
+print(my_list)
+
+#creating a table with list
+# mylist = [[i for i in range (4)] for j in range (3)]
+# print (mylist)
+
